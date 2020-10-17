856f53c81db250c5e19877b49d176757d97cd141
LadyHail <ladydeath@o2.pl>
2020-10-17 18:22:10 +0200

    Init

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace DebuggerDisplayAttributeDemo
+{
+    [DebuggerDisplay("{Id}; {Pages}", Name = "{Title} / {Author}")]
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int Pages { get; set; }
+
+        public Book(int id, string title, string author, int pages)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            Pages = pages;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DebuggerDisplayAttributeDemo
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var books = new List<Book>
+            {
+                new Book(0, "First", "A", 50),
+                new Book(1, "Second", "B", 100),
+                new Book(2, "Third", "C", 150),
+                new Book(3, "Fourth", "D", 200),
+                new Book(4, "Fifth", "E", 250),
+                new Book(5, "Sixth", "F", 300),
+                new Book(6, "Seventh", "G", 350),
+                new Book(7, "Eighth", "H", 400),
+                new Book(8, "Ninth", "I", 450),
+            };
+
+            Console.WriteLine(books);
+            Console.ReadKey();
         }
     }
 }
